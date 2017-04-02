56a0f7d54fbd1a7c84519cbaf973a56bba4233b3
Alec Minchington <slarty@bardfarst.com>
2017-04-01 23:55:25 -0400

    Added backout binding

--- 2017RobotCode/src/org/usfirst/frc/team226/robot/OI.java
+++ 2017RobotCode/src/org/usfirst/frc/team226/robot/OI.java
@@ -4,6 +4,7 @@ package org.usfirst.frc.team226.robot;
 import static org.usfirst.frc.team226.robot.RobotMap.DRIVER_CONTROLLER;
 import static org.usfirst.frc.team226.robot.RobotMap.MANIP_CONTROLLER;
 
+import org.usfirst.frc.team226.robot.autons.grpBackOut2;
 import org.usfirst.frc.team226.robot.commands.cmdExpandRobot;
 import org.usfirst.frc.team226.robot.commands.cmdMoveLeftAgitator_button;
 import org.usfirst.frc.team226.robot.commands.cmdMoveLeftFeeder_button;
@@ -11,9 +12,6 @@ import org.usfirst.frc.team226.robot.commands.cmdMoveRightAgitator_button;
 import org.usfirst.frc.team226.robot.commands.cmdMoveRightFeeder_button;
 import org.usfirst.frc.team226.robot.commands.cmdPIDLeftShooter;
 import org.usfirst.frc.team226.robot.commands.cmdPIDRightShooter;
-import org.usfirst.frc.team226.robot.commands.cmdPIDTurnToAngle;
-import org.usfirst.frc.team226.robot.commands.cmdPIDTurnToAngleCurved;
-import org.usfirst.frc.team226.robot.commands.cmdResetRobot;
 import org.usfirst.frc.team226.robot.extlib.Controller;
 
 
@@ -28,6 +26,8 @@ public class OI {
 	
 	public OI() {
 		
+		driver.getAButton().whenPressed(new grpBackOut2());
+		
 		manip.getAButton().whenPressed(new cmdPIDLeftShooter(RobotMap.L_SHOOTER_SETPOINT)); //3075 maybe decrease to 3000?
 		manip.getAButton().whenPressed(new cmdPIDRightShooter(RobotMap.R_SHOOTER_SETPOINT)); //3150 maybe decrease to 3075?
 		
@@ -36,18 +36,7 @@ public class OI {
 		manip.getYButton().whileHeld(new cmdMoveRightAgitator_button(3, 0.5, 0.5, 0.75));
 		manip.getYButton().whileHeld(new cmdMoveLeftAgitator_button(3, 0.5, 0.5, 0.75));
 		
-//		driver.getAButton().whenPressed(new cmdPIDDriveInches(-75.0, 0.7));
-//		driver.getXButton().whenPressed(new cmdPIDDriveInches(-19.0, 0.7));
-//		driver.getYButton().whenPressed(new cmdResetDTSensors());
-		
-		driver.getAButton().whenPressed(new cmdPIDTurnToAngleCurved(35, 0.72, 0.68));
-		driver.getXButton().whenPressed(new cmdPIDTurnToAngle(32, 0.7));
-		driver.getYButton().whenPressed(new cmdPIDTurnToAngleCurved(75, 0.75, 0.85));
-		
-//		driver.getAButton().whenPressed(new cmdPIDTurnWithVision());
-		
 		manip.getSTARTButton().whenPressed(new cmdExpandRobot());
-		manip.getBACKButton().whenPressed(new cmdResetRobot());
 	  	
 	}
 	
